27876528182e00c9aaa96fe2db7b46d3096dab49
metastable-void <git@yuka.h09.eu>
2026-03-23 15:30:16 +0900

    increase limits

--- src/bin/mechanics-rs.rs
+++ src/bin/mechanics-rs.rs
@@ -11,9 +11,9 @@ fn main() -> std::io::Result<Infallible> {
         SocketAddr::from_str(&std::env::var("LISTEN_ADDR").unwrap_or("".to_string()))
             .unwrap_or(SocketAddr::from(([127, 0, 0, 1], 3001)));
     let mut config = MechanicsPoolConfig::default();
-    config = config.with_execution_limits(MechanicsExecutionLimits::new(Duration::from_secs(60), 65536, 65536, 131072).map_err(std::io::Error::other)?);
-    config = config.with_run_timeout(Duration::from_secs(180));
-    config = config.with_default_http_timeout_ms(Some(120_000));
+    config = config.with_execution_limits(MechanicsExecutionLimits::new(Duration::from_secs(3600), 65536, 65536, 131072).map_err(std::io::Error::other)?);
+    config = config.with_run_timeout(Duration::from_secs(3600));
+    config = config.with_default_http_timeout_ms(Some(300_000));
     let server = MechanicsServer::new(config)?;
     let mut token_count = 0usize;
     if let Ok(tokens) = std::env::var("MECHANICS_ALLOWED_TOKENS") {
